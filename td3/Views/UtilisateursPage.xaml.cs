4703b40e1a7d9b43160a1e579a761ea18c429c66
Maxime Girard <mx.girard42@gmail.com>
2019-04-29 11:06:28 +0200

    préparation de l'application mobile

---
+++ td3/Views/UtilisateursPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace td3.Views
+{
+    public partial class UtilisateursPage : ContentPage
+    {
+        public UtilisateursPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

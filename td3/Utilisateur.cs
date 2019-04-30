b0d02d073b120c46a07185b6b2620eda63d52ad6
Maxime Girard <mx.girard42@gmail.com>
2019-04-30 09:47:32 +0200

    WSUtilisateur creation de GetUtilisateurAsync

---
+++ td3/Utilisateur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace td3
+{
+    public class Utilisateur
+    {
+        public double id { get; set; }
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public string email { get; set; }
+        public string rue { get; set; }
+        public string cp { get; set; }
+        public string ville { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ td3/WSUtilisateurs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace td3
+{
+    public class WSUtilisateurs
+    {
+        private static WSUtilisateurs _instance;
+        private readonly HttpClient _httpClient;
+
+        public static WSUtilisateurs Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new WSUtilisateurs();
+                }
+                return _instance;
+            }
+        }
+
+        private WSUtilisateurs()
+        {
+            this._httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://vcouturier-001-site13.ctempurl.com/api/");
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<Utilisateur> GetUtilisateurAsync(string email, string password)
+        {
+            Utilisateur user = null;
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync("utilisateurs?email=" + email + "&password=" + password);
+                if (response.IsSuccessStatusCode)
+                {
+                    var contenu = await response.Content.ReadAsStringAsync();
+                    user = JsonConvert.DeserializeObject<Utilisateur>(contenu);
+                }
+            }
+            catch (Exception)
+            {
+                await Xamarin.Forms.Application.Current.MainPage.DisplayAlert("Erreur", "Erreur de connexion au WS !", "Ok");
+            }
+
+            return user;
+        }
+    }
+}
